b9960c72d5420dda327c21f836372356ef7d85dc
sapux04 clouddnagmbhgmail.onmicrosoft.com <sapux04@clouddnagmbhgmail.onmicrosoft.com>
2022-10-13 13:52:21 +0000

    Delete entry with seperate column

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,14 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
 ],
-    function (Controller) {
+    function (Controller,  MessageToast, MessageBox) {
+
         "use strict";
 
         return Controller.extend("at.clouddna.student04.zhoiu5vn.controller.Main", {
 
             onInit: function(){
 
-            }
+            },
+            
+            onDeleteButtonPressed: function(oEvent){
+                let oModel = this.getView().getModel();
+                let oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            
+                let oSource = oEvent.getSource();
+                let sPath = oSource.getBindingContext().getPath();
+            
+                MessageBox.warning(oResourceBundle.getText("sureToDeleteQuestion"), {
+                    title: oResourceBundle.getText("sureToDeleteTitle"),
+                    actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                    emphasizedAction: MessageBox.Action.YES,
+                    onClose: function(oAction){
+                        if(MessageBox.Action.YES === oAction){
+                            oModel.remove(sPath);
+                        }
+                    }
+                });
+            },
             
         });
     });
