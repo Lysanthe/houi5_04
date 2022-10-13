8d5453f7156f5eead97139bacdd187a4b22248b4
sapux04 clouddnagmbhgmail.onmicrosoft.com <sapux04@clouddnagmbhgmail.onmicrosoft.com>
2022-10-13 09:45:08 +0000

    Table implemented

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageBox) {
         "use strict";
 
         return Controller.extend("at.clouddna.student04.zhoiu5vn.controller.Customer", {
@@ -17,10 +18,21 @@ sap.ui.define([
                 let iO18nModel = oView.getModel("i18n");
                 let oResoureceBoundle = iO18nModel.getResourceBundle();
                 //debugger;
-                console.log( "hello world", sKey);
+                //console.log( "hello world", sKey);
                 let sText = oResoureceBoundle.getText(sKey["value"]);
 
                 return sText;
+            },
+
+            onSavePressed: function() {
+                let oView = this.getView();
+                let oModel = oView.getModel();
+                let oData = oModel.getData();
+
+                console.log(oData);
+               // alert("hello world");
+
+               MessageBox.success(JSON.stringify(oData));
             }
         });
     });
---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("at.clouddna.student04.zhoiu5vn.controller.Main", {
+
+            onInit: function(){
+
+            }
+            
+        });
+    });
